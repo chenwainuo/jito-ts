32d2f97414cca5ef782f052f9abbb0267a1151c7
Chen Wainuo <83727873+chenwainuo@users.noreply.github.com>
2024-01-29 20:45:25 -0800

    raw

--- src/sdk/block-engine/searcher.ts
+++ src/sdk/block-engine/searcher.ts
@@ -22,6 +22,7 @@ import {
 import {authInterceptor, AuthProvider} from './auth';
 import {Bundle} from './types';
 import {deserializeTransactions} from './utils';
+import {Packet} from "../../gen/block-engine/packet";
 
 export class SearcherClient {
   private client: SearcherServiceClient;
@@ -152,7 +153,7 @@ export class SearcherClient {
   onProgramUpdate(
     programs: PublicKey[],
     regions: string[],
-    successCallback: (transactions: VersionedTransaction[]) => void,
+    successCallback: (transactions: Packet[]) => void,
     errorCallback: (e: Error) => void
   ): () => void {
     const stream: ClientReadableStream<PendingTxNotification> =
@@ -166,7 +167,7 @@ export class SearcherClient {
     stream.on('readable', () => {
       const msg = stream.read(1);
       if (msg) {
-        successCallback(deserializeTransactions(msg.transactions));
+        successCallback(msg.transactions);
       }
     });
     stream.on('error', e => {
